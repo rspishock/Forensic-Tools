51729401945c532a32c564f4f2860b2593983618
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2022-07-30 22:30:14 -0400

    Initial commit

---
+++ registry.bat
@@ -0,0 +1,16 @@
+@echo off
+
+echo "Creating file structure..."
+md Backup
+md Text_files
+
+echo "Creating a backup of the registry hives..."
+copy * Backup
+
+echo "Parsing registry files..."
+for /r %%i in (*) do (C:\Tools\RegRipper\rip.exe -r %%i -a > %%i.txt)
+
+echo "Moving text files to 'Text_files'..."
+move *.txt Text_files
+
+echo "Process completed successfully"
---
+++ setup.bat
@@ -0,0 +1,7 @@
+@echo off
+
+echo Creating directories...
+md C:\Case
+md C:\Tools
+
+rem Copy tools from remote server
