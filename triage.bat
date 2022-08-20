8dbefffc1a05f4d45a056a5f8ef48e01aa42dd6b
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2022-08-20 13:55:02 -0400

    Add two new triage scripts

---
+++ triage.bat
@@ -0,0 +1,37 @@
+@echo off
+
+rem setup environment
+DIRECTORY = C:\Users\%CurrentUser%\%DATE%_%TIME%\
+md %DIRECTORY%
+cd %DIRECTORY%
+
+rem output ipconfig data to a file
+ipconfig /a >> ip.txt
+echo Acquired: %TIME% on %DATE$ >> ip.txt
+
+rem output running processes to a file
+tasklist >> tasklist.txt
+echo Acquired: %TIME% on %DATE$ >> tasklist.txt
+
+rem output running processes and the associated binary to a file
+echo Verify any files running out of /tmp/ or /Downloads/ >> full_tasklist.txt
+echo >> full_tasklist.txt
+echo >> full_tasklist.txt
+wmic process get description, executablepath >> full_tasklist.txt
+echo Acquired: %TIME% on %DATE$ >> full_tasklist.txt
+
+rem output a list of detailed services to a file
+sc query | more >> services.txt
+echo Acquired: %TIME% on %DATE$ >> services.txt
+
+rem output a list of all users with local accounts on this system to a file
+netuser >> users.txt
+echo Acquired: %TIME% on %DATE$ >> users.txt
+
+rem output a list of all users in the local admin group
+net localgroup administrators >> admins.txt
+echo Acquired: %TIME% on %DATE$ >> admins.txt
+
+rem output a list of all open ports to a file
+netstat -ab >> ports.txt
+echo Acquired: %TIME% on %DATE$ >> ports.txt
---
+++ triage.ps1
@@ -0,0 +1,21 @@
+# setup environment
+DIRECTORY = C:\Users\%CurrentUser%\%DATE%_%TIME%\
+md %DIRECTORY%
+cd %DIRECTORY%
+
+# output ipconfig data to a file
+Get-NetIPAddress | Add-Content "IP.txt"
+
+
+# output a list of all users with local accounts on this system to a file
+netuser >> users.txt
+echo Acquired: %TIME% on %DATE$ | Add-Content "users.txt"
+
+# output a list of detailed services to a file
+Get-Service | Where Status -eq "Running" | Out-GridView | Add-Content "services.txt"
+
+# output a list of rinning services by priority to a file
+Get-Process | Format-Table -View priority | Add-Content "services_priority.txt"
+
+# output a list of scheduled tasks to a file
+Get-ScheduledTask | Add-Content "schedule.txt"
